7f6dda02daa27d11aeb2b934da65e082d7c40c98
Boris Vargas <borisvargaspaucara@gmail.com>
2022-12-09 07:00:45 -0400

    subiendo cmabios errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './components/home/home.component';
+import { DialogAnimationsExampleDialog, HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -35,7 +35,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     HomeComponent,
     DashboardComponent,
     AddressFormComponent,
-    TableComponent
+    TableComponent,
+    DialogAnimationsExampleDialog
   ],
   imports: [
     BrowserModule,
